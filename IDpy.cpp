bd74d4806e32086240dbd1d89df66d382d977f01
taka <1201201knct49@gmail.com>
2020-10-12 23:13:59 +0900

    invdSolvenuをnewするとImportError

--- IDpy.cpp
+++ IDpy.cpp
@@ -35,33 +35,40 @@
 #include "inversedynamics.h"
 #include "IDpy.h"
 
-class stddvector {
+class ArioID{
 public:
     typedef std::vector<int> int_vector;
     typedef std::vector<double> double_vector;
 public:
+    ArioID();
     double_vector const& get_list(double_vector const &a) const {
         return a;
     }
 private:
     int_vector v_;
     double_vector vd_;
+    invdSolvenu *maninvd;
 };
+
+ArioID::ArioID(){
+    int ii,jointn = 7;
+    maninvd = new invdSolvenu(jointn);
+}
 
 
 BOOST_PYTHON_MODULE( IDpy ){
     using namespace boost::python;
 
-    class_<stddvector>("stddvector")
-        .def("get_list", &stddvector::get_list, return_value_policy<copy_const_reference>());
-    to_python_converter<stddvector::int_vector, vector_to_pylist_converter<stddvector::int_vector> >();
+    class_<ArioID>("ArioID")
+        .def("get_list", &ArioID::get_list, return_value_policy<copy_const_reference>());
+    to_python_converter<ArioID::int_vector, vector_to_pylist_converter<ArioID::int_vector> >();
     converter::registry::push_back(
-        &pylist_to_vector_converter<stddvector::int_vector>::convertible,
-        &pylist_to_vector_converter<stddvector::int_vector>::construct,
-        boost::python::type_id<stddvector::int_vector>());
-    to_python_converter<stddvector::double_vector, vector_to_pylist_converter<stddvector::double_vector> >();
+        &pylist_to_vector_converter<ArioID::int_vector>::convertible,
+        &pylist_to_vector_converter<ArioID::int_vector>::construct,
+        boost::python::type_id<ArioID::int_vector>());
+    to_python_converter<ArioID::double_vector, vector_to_pylist_converter<ArioID::double_vector> >();
     converter::registry::push_back(
-        &pylist_to_vector_converter<stddvector::double_vector>::convertible,
-        &pylist_to_vector_converter<stddvector::double_vector>::construct,
-        boost::python::type_id<stddvector::double_vector>());
+        &pylist_to_vector_converter<ArioID::double_vector>::convertible,
+        &pylist_to_vector_converter<ArioID::double_vector>::construct,
+        boost::python::type_id<ArioID::double_vector>());
 }
