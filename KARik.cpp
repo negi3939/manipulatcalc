88f40bc0becd65b84d1c557ca35c626ffabab68e
taka <1201201knct49@gmail.com>
2020-11-04 21:43:33 +0900

    KAR DH comp

--- KARik.cpp
+++ KARik.cpp
@@ -36,17 +36,24 @@ using namespace Mymath;
 
 int main(){
     int ii,jointn = 6;
+    Matrix4d mattheta;
     invkSolvenu *maninvk;
     maninvk = new invkSolvenu(jointn);
     /*KAR*/
-    //                      jointnum     thetaoff                       aa                di           alpha
-    maninvk->setdhparameter(0,           0.0d*M_PI,                              0.055d, 0.21d,  -0.5d*M_PI);//(int num,double thoff,double aa,double di,double alph);
-    maninvk->setdhparameter(1,atan2(0.080d,0.420d), sqrt(0.420d*0.420d + 0.080d*0.080d),  0.0d,   0.0d*M_PI);//(int num,double thoff,double aa,double di,double alph);
-    maninvk->setdhparameter(2,           0.0d*M_PI,                              0.390d,  0.0d,   0.5d*M_PI);//(int num,double thoff,double aa,double di,double alph);
-    maninvk->setdhparameter(3,           0.0d*M_PI,                                0.0d,  0.0d,  -0.5d*M_PI);//(int num,double thoff,double aa,double di,double alph);
-    maninvk->setdhparameter(4,           0.0d*M_PI,                              0.045d,  0.0d, 0.5d*M_PI);//(int num,double thoff,double aa,double di,double alph);
-    maninvk->setdhparameter(5,           0.0d*M_PI,                                0.0d,  0.0d, 0.0d*M_PI);//(int num,double thoff,double aa,double di,double alph);
+    double attachdis = 0.102d;
+    //                      jointnum             thetaoff                       aa                    di           alpha
+    maninvk->setdhparameter(0,                       0.0d*M_PI,                              0.055d, 0.21d,   0.5d*M_PI);//(int num,double thoff,double aa,double di,double alph);
+    maninvk->setdhparameter(1,0.5d*M_PI- atan2(0.080d,0.420d), sqrt(0.420d*0.420d + 0.080d*0.080d),  0.0d,   -1.0d*M_PI);//(int num,double thoff,double aa,double di,double alph);
+    maninvk->setdhparameter(2,           -atan2(0.080d,0.420d),                             0.390d,  0.0d,   0.5d*M_PI);//(int num,double thoff,double aa,double di,double alph);
+    maninvk->setdhparameter(3,                       0.0d*M_PI,                                0.0d,  0.0d,  -0.5d*M_PI);//(int num,double thoff,double aa,double di,double alph);
+    maninvk->setdhparameter(4,                       0.0d*M_PI,                  0.045d + attachdis,  0.0d,   0.5d*M_PI);//(int num,double thoff,double aa,double di,double alph);
+    maninvk->setdhparameter(5,                       0.0d*M_PI,                                0.0d,  0.0d,   0.0d*M_PI);//(int num,double thoff,double aa,double di,double alph);
     //limit add
+    VectorXd angle = VectorXd::Zero(6);
+    angle(2) = 0.5d*M_PI;
+    maninvk->calcaA(angle,mattheta);
+    PRINT_MAT(mattheta);
+    std::cout << "atan2 val "<< (-0.5d*M_PI+ atan2(0.080d,0.420d))*180/M_PI << std::endl;
     VectorXd uplimit(6);
     VectorXd lowlimit(6);
     uplimit <<    2.72271 ,  0.5*M_PI  ,   2.72271  ,        0 ,   2.72271 ,    0.5*M_PI;//可動上限範囲を設定
